e97385ad0b69d0f789576f2d969329d9052565c1
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2018-04-21 22:22:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const Koa = require('koa')
 const app = new Koa()
 const colors = require('./fakes/colors.json')
 
-const port = process.env.PORT | 3000
+const port = process.env.PORT || 3000
 
 //This API will give list of different colors
 app.use(async ctx => {
-    ctx.body = colors;
-});
+    ctx.body = colors
+})
 
 app.listen(port, () => {
     console.log('Server is running on PORT ' + port)
-});
+})
